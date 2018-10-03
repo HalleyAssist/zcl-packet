74166ef7a4c5a3240ede9d89276e312f5abd4c6e
Mathew Heard <splitice@users.noreply.github.com>
2018-10-03 14:39:57 +1000

    use the same format for meta as cc-znp
    ids not strings

--- convert.js
+++ convert.js
@@ -1,12 +1,27 @@
-var zpiMeta = require('./lib/defs/zcl_meta.json')
+var zpiMeta = require('./lib/defs/zcl_meta.json'),
+    Enum = require('light-enum'),
+    zclDefs = require('./lib/defs/zcl_defs.json')
+var paramsType = new Enum(zclDefs.ParamType)
 
-function process(params){
+function process1(params){
     var output = []
     for(var i in params){
         var p = params[i]
-        output.push([Object.keys(p)[0], Object.values(p)[0]])
+        if(p instanceof Array) output.push(p)
+        else output.push([Object.keys(p)[0], Object.values(p)[0]])
     }
     return output
+}
+
+function process(params){
+    params = process1(params)
+    for(var i in params){
+        var r = params[i]
+        var got = paramsType.get(r[1])
+        if(got) r[1] = got.value
+        else console.log(r[1])
+    }
+    return params
 }
 
 let subsys = zpiMeta.foundation
@@ -22,5 +37,7 @@ for(var i in zpiMeta.functional){
         cmd.params = process(cmd.params)
     }
 }
+
+
 
 console.log(JSON.stringify(zpiMeta))
